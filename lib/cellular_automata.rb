2cd054925e0b397a24ec8cae5857efa09ae60080
Forrest Fleming <ffleming@gmail.com>
2015-05-24 17:00:57 -0700

    Gif writer

--- lib/cellular_automata.rb
+++ lib/cellular_automata.rb
@@ -2,11 +2,13 @@ require 'pry'
 require 'byebug'
 require 'pry-byebug'
 require 'gosu'
+require 'rmagick'
 require "cellular_automata/version"
 require "cellular_automata/cell"
 require "cellular_automata/rule"
 require "cellular_automata/board"
 require "cellular_automata/gui_window"
+require "cellular_automata/gif_writer"
 
 module CellularAutomata
 end
