3cb8327d6d0a1f089a741f7794722cb05b44adcb
Forrest Fleming <ffleming@gmail.com>
2015-05-25 12:29:15 -0700

    More speed improvements
    This code is getting ugly in the name of speed.  Welp.

--- lib/cellular_automata/board.rb
+++ lib/cellular_automata/board.rb
@@ -27,8 +27,7 @@ class CellularAutomata::Board
   def tick!
     next_state = Marshal.load(Marshal.dump @state)
     each_cell do |x, y|
-      result = rule.process(neighbor_population_of(x: x, y: y)) #= next_cell #cell.send(rule.process(adj_pop))
-      # next_state[y][x].send 
+      result = rule.process(neighbor_population_of(x: x, y: y))
       next_state[y][x] = 0 if result == :die!
       next_state[y][x] = 1 if result == :live!
     end
@@ -64,29 +63,24 @@ class CellularAutomata::Board
     (0..height-1).each do |y|
       arr[y] = []
       (0..width-1).each do |x|
-        arr[y][x] = 0 # CellularAutomata::Cell.new(row: y, column: x, alive: false)
+        arr[y][x] = 0
       end
     end
     return arr
   end
 
   def neighbor_population_of(x: , y: )
-    # neighbors_of(x: x, y: y).select {|c| c == true}.length
-    neighbors_of(x:x, y: y).inject(:+)
+    ret = 0
+    min_x = x > 1 ? x - 1 : 0
+    max_x = x < width - 2 ? x + 1 : width - 1
+    min_y = y > 1 ? y - 1 : 0
+    max_y = y < height - 2 ? y + 1 : height - 1
+    (min_y..max_y).each do |row|
+      (min_x..max_x).each do |col|
+        ret += 1 unless ((x == col && y == row) || @state[row][col] == 0)
+      end
+    end
+    ret
   end
-
-  def cell_at(y, x)
-    return nil if x  < 0 || y < 0
-    return nil if y > height - 1
-    return nil if x > width - 1
-    return @state[y][x]
-  end
-
-  def neighbors_of(x: , y: )
-    [ cell_at(y-1, x-1), cell_at(y-1, x  ), cell_at(y-1, x+1),
-      cell_at(y,   x+1),                    cell_at(y  , x-1),
-      cell_at(y+1, x-1), cell_at(y+1, x  ), cell_at(y+1, x+1) ].compact
-  end
-
 end
 
